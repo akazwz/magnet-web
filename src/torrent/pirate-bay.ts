f56167cefe9a6bb0623ab7e693944b0f8de7950c
akazwz <akazwz@pm.me>
2022-03-22 20:37:55 +0800

    get torrents (dev and prod)

--- src/torrent/pirate-bay.ts
+++ src/torrent/pirate-bay.ts
@@ -12,21 +12,40 @@ export type Torrent = {
   Magnet: string | undefined
 }
 
+const getHtml = async (url: string): Promise<string | null> => {
+  /* 开发环境 */
+  if (process.env.NODE_ENV === 'development') {
+    try {
+      const puppeteer = require('puppeteer')
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(url)
+      return await page.content()
+    } catch (e: unknown) {
+      console.log(e)
+      return null
+    }
+  } else {
+    /* 生产环境 */
+    try {
+      const res = await fetch(url)
+      return await res.text()
+    } catch (e: unknown) {
+      console.log(e)
+      return null
+    }
+  }
+}
+
 export const pirateBay = async (query: string, page: number = 1) => {
   const torrents: Torrent[] = []
   const url = 'https://thehiddenbay.com/search/' + query + '/' + page + '/99/0'
-  let html: string
-  try {
-    const res = await fetch(url)
-    html = await res.text()
-  } catch (e: unknown) {
-    console.log(e)
-    return null
-  }
-
+  const html = await getHtml(url)
+  if (!html) return null
+  console.log(url)
   const $ = cheerio.load(html)
 
-  $('table#searchResult > tr').each((_, element) => {
+  $('table#searchResult tr').each((_, element) => {
     const data = $(element).find('font.detDesc').text().replace(/(Size|Uploaded)/gi, '').replace(/ULed/gi, 'Uploaded').split(',').map(value => value.trim())
     const date = data[0]
     const size = data[1]
